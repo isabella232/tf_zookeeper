caed0664065b7671c0913409845638fd967ab3f9
Cihangir SAVAS <cihangir@koding.com>
2016-06-04 23:59:44 +0300

    template: creaste templates with count param

--- templates.tf
+++ templates.tf
@@ -1,7 +1,10 @@
 resource "template_file" "zookeeper_cloud_init_file" {
   template = "${file("cloud_init/zookeeper.yaml")}"
+  count    = "${var.instance_count}"
 
   vars = {
-    REGION = "${var.region}"
+    self_private_ip = "${element(aws_eip.zookeeper.*.private_ip, count.index)}"
+    private_ips     = "${join(",", aws_eip.zookeeper.*.private_ip)}"
+    region          = "${var.region}"
   }
 }
